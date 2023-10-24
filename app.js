8e80a250b20f61532972f67f19bc03e195870887
Ricardo Moraes <ricardo.fgm77@gmail.com>
2023-10-23 21:10:48 -0300

    feat: Adicionando dicas e botões

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-let titulo = document .querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto.';
+let numeroSecreto = numeroAleatorio();
+let tentativas = 1
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um nuúmero entre 1 e 10';
+function exibirTextoNaTela (tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function mensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+
 
 function verificarChute(){
-    console.log('O botão foi clicado');
-};
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; 
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'Número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    
+    }
+}
+
+
+function  numeroAleatorio(){
+    return parseInt (Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = numeroAleatorio();
+    limparCampo();
+    tentativas =1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
